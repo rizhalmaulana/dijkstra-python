fa106d71f38bd056a7bd12b4efc79eeaef235838
rizhalmaulana <4s3tk1t4@gmail.com>
2024-05-07 17:39:30 +0700

    fix: add vertex in index.html and add icon flood and start navigator

--- scripts/marker.js
+++ scripts/marker.js
@@ -1,41 +1,60 @@
 // Node
 // Koordinat di map
 nodes = [
-    [-6.302286939445012, 107.30263666788876, "Dinas Sosial Kabupaten Karawang"],
-    [-6.274444041783581, 107.27545081274539, "Kecamatan Karawang Barat"],
-    [-6.248831041245198, 107.35653204146328, "Kecamatan Rawamerta"],
-    [-6.15935581925213, 107.29088019953244, "Kecamatan Rengasdengklok"],
-    [-6.174363327404389, 107.33598719471847, "Kecamatan Kutawaluya"],
-    [-6.0765766075880565, 107.17678474393144, "Kecamatan Batujaya"],
-    [-6.046051980055279, 107.1401055970806, "Kecamatan Pakisjaya"],
-    [-6.048595402686564, 107.25020169825245, "Kecamatan Tirtajaya"],
-    [-6.094973198447335, 107.36893046588209, "Kecamatan Pedes"],
-    [-6.1421735155842105, 107.40973662692691, "Kecamatan Cilebar"],
-    [-6.235049237677933, 107.51855001258467, "Kecamatan Cilamaya Kulon"],
-    [-6.250756841591445, 107.58080332809607, "Kecamatan Cilamaya Wetan"],
-    [-6.309397482870587, 107.53494441006194, "Kecamatan Banyusari"],
-    [-6.356352947008921, 107.46870259952021, "Kecamatan Tirtamulya"],
-    [-6.371694299019733, 107.52209174154964, "Kecamatan Jatisari"],
-    [-6.400298850939683, 107.48061482133745, "Kecamatan Kotabaru"],
-    [-6.400914495683963, 107.44300602792109, "Kecamatan Cikampek"],
-    [-6.379591217772605, 107.41757553952661, "Kecamatan Purwasari"],
-    [-6.368513084841798, 107.36979778568171, "Kecamatan Klari"],
-    [-6.38959792989387, 107.36086324124643, "Kecamatan Ciampel"],
-    [-6.51583416423437, 107.22937214150302, "Kecamatan Tegalwaru"],
-    [-6.467829518127596, 107.21561637016019, "Kecamatan Pangkalan"],
-    [-6.338338022071176, 107.25161466997534, "Kecamatan Telukjambe Barat"],
-    [-6.337512560944032, 107.31156939287322, "Kecamatan Telukjambe Timur"],
-    [-6.316661211459145, 107.3220346235559, "Kecamatan Karawang Timur"],
+    [-6.302286939445012, 107.30263666788876, "[V0] Dinas Sosial Kabupaten Karawang"],
+    [-6.274444041783581, 107.27545081274539, "[V1] Kecamatan Karawang Barat"],
+    [-6.248831041245198, 107.35653204146328, "[V2] Kecamatan Rawamerta"],
+    [-6.15935581925213, 107.29088019953244, "[V3] Kecamatan Rengasdengklok"],
+    [-6.174363327404389, 107.33598719471847, "[V4] Kecamatan Kutawaluya"],
+    [-6.0765766075880565, 107.17678474393144, "[V5] Kecamatan Batujaya"],
+    [-6.046051980055279, 107.1401055970806, "[V6] Kecamatan Pakisjaya"],
+    [-6.048595402686564, 107.25020169825245, "[V7] Kecamatan Tirtajaya"],
+    [-6.094973198447335, 107.36893046588209, "[V8] Kecamatan Pedes"],
+    [-6.1421735155842105, 107.40973662692691, "[V9] Kecamatan Cilebar"],
+    [-6.235049237677933, 107.51855001258467, "[V10] Kecamatan Cilamaya Kulon"],
+    [-6.250756841591445, 107.58080332809607, "[V11] Kecamatan Cilamaya Wetan"],
+    [-6.309397482870587, 107.53494441006194, "[V12] Kecamatan Banyusari"],
+    [-6.356352947008921, 107.46870259952021, "[V13] Kecamatan Tirtamulya"],
+    [-6.371694299019733, 107.52209174154964, "[V14] Kecamatan Jatisari"],
+    [-6.400298850939683, 107.48061482133745, "[V15] Kecamatan Kotabaru"],
+    [-6.400914495683963, 107.44300602792109, "[V16] Kecamatan Cikampek"],
+    [-6.379591217772605, 107.41757553952661, "[V17] Kecamatan Purwasari"],
+    [-6.368513084841798, 107.36979778568171, "[V18] Kecamatan Klari"],
+    [-6.38959792989387, 107.36086324124643, "[V19] Kecamatan Ciampel"],
+    [-6.51583416423437, 107.22937214150302, "[V20] Kecamatan Tegalwaru"],
+    [-6.467829518127596, 107.21561637016019, "[V21] Kecamatan Pangkalan"],
+    [-6.338338022071176, 107.25161466997534, "[V22] Kecamatan Telukjambe Barat"],
+    [-6.337512560944032, 107.31156939287322, "[V23] Kecamatan Telukjambe Timur"],
+    [-6.316661211459145, 107.3220346235559, "[V24] Kecamatan Karawang Timur"],
 ];
 
 function addAllMarkers() {
     deleteAllMarker();
 
+    var floodIcon = L.icon({
+        iconUrl: 'assets/flood.png',
+        iconSize: [50, 50]
+    });
+
+    var navigatorIcon = L.icon({
+        iconUrl: 'assets/navigator.png',
+        iconSize: [50, 50]
+    });
+
     $.each(nodes, function(index, value) {
-        L.marker(L.latLng(
-            parseFloat(value[0]),
-            parseFloat(value[1])
-          )).addTo(markers).bindPopup(value[2]);
+        if (value[2] != "[V0] Dinas Sosial Kabupaten Karawang") {
+            L.marker(L.latLng(
+                parseFloat(value[0]),
+                parseFloat(value[1])), 
+                {icon: floodIcon}
+            ).addTo(markers).bindPopup(value[2]);
+        } else {
+            L.marker(L.latLng(
+                parseFloat(value[0]),
+                parseFloat(value[1])), 
+                {icon: navigatorIcon}
+            ).addTo(markers).bindPopup(value[2]);
+        }
     });
 
     addAllPath();
@@ -65,6 +84,7 @@ function addMarker() {
         checkDistance(start, end);
 
         $.get(`http://192.168.32.228:8005/path?start=${start}&end=${end}`, function(data) {
+            convertLintasanVertex(data.lintasan);
             addPath(data.lintasan);
         });
     }
@@ -78,12 +98,31 @@ function checkDistance(start, end) {
 }
 
 function addPath(path) {
+    var floodIcon = L.icon({
+        iconUrl: 'assets/flood.png',
+        iconSize: [50, 50]
+    });
+
+    var navigatorIcon = L.icon({
+        iconUrl: 'assets/navigator.png',
+        iconSize: [50, 50]
+    });
+
     $.each(path, function(i, value) {
-        L.marker(L.latLng(nodes[value])).addTo(markers).bindPopup(nodes[value][2]);
+        if (nodes[value][2] != "[V0] Dinas Sosial Kabupaten Karawang") {
+            L.marker(L.latLng(nodes[value]), {icon: floodIcon}).addTo(markers).bindPopup(nodes[value][2]);
+        } else {
+            L.marker(L.latLng(nodes[value]), {icon: navigatorIcon}).addTo(markers).bindPopup(nodes[value][2]);
+        }
         L.polygon([nodes[value], nodes[path[i + 1]]]).addTo(markers);
     });
 }
 
 function deleteAllMarker() {
     markers.clearLayers()
+}
+
+function convertLintasanVertex(lintasan) {
+    const result = lintasan.map(number => `[V${number}]`).join(' - ');
+    $('#result-vertex').val(result);
 }
